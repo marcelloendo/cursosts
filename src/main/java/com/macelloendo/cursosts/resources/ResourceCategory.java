62ffdd8d9350540b35d5c08562f1cf48e3791dad
Marcello Hideyuki Endo <marcelloendo@hotmail.com>
2020-04-14 23:41:59 -0300

    test first class

--- src/main/java/com/macelloendo/cursosts/resources/ResourceCategory.java
+++ src/main/java/com/macelloendo/cursosts/resources/ResourceCategory.java
@@ -1,16 +1,29 @@
 package com.macelloendo.cursosts.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.macelloendo.cursosts.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class ResourceCategory {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST OK!";
+	public List<Category> list() {
+
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 
 }
