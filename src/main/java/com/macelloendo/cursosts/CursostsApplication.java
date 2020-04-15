ba4f94a58cd0f5d8aeceb0e75c0e106b981a4fa8
Marcello Hideyuki Endo <marcelloendo@hotmail.com>
2020-04-14 23:11:30 -0300

    INIT

---
+++ src/main/java/com/macelloendo/cursosts/CursostsApplication.java
@@ -0,0 +1,13 @@
+package com.macelloendo.cursosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macelloendo/cursosts/CursostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macelloendo.cursosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
