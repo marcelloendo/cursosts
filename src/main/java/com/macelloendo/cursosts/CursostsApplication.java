2ab3d631125cf5136635d5445a2629a364284891
Marcello Hideyuki Endo <marcelloendo@hotmail.com>
2020-04-15 22:23:32 -0300

    create operation instantiation

--- src/main/java/com/macelloendo/cursosts/CursostsApplication.java
+++ src/main/java/com/macelloendo/cursosts/CursostsApplication.java
@@ -1,14 +1,34 @@
 package com.macelloendo.cursosts;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.macelloendo.cursosts.domain.Category;
+import com.macelloendo.cursosts.repository.CategoryRepository;
+
 @SpringBootApplication
-public class CursostsApplication {
+public class CursostsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursostsApplication.class, args);
 		System.out.println("*****************STARTED*****************");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
